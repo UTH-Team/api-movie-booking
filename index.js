1184d4ce08afe90a4561a3b2ee118fc662959aac
trdoan <tranduongdoan0922@gmail.com>
2022-10-09 10:06:15 +0700

    use app controller

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const {errorHandler, responseFormat} = require('./controllers/app.controller')
 const rootRouter = require('./routers/root.router')
 const app = express()
 const port = process.env.PORT || 3012
@@ -7,6 +8,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 app.use("/", rootRouter);
+app.use("/", errorHandler, responseFormat)
 app.listen(port, () => {
   console.log(`API for movie booking ticket is ready on ${port}`)
 })
