ce3f6f2d5fd690fdecfc7bb67ccb174b28cf14a0
CongTan2661 <congtan3081@gmail>
2022-11-14 21:35:48 +0700

    add auth router

--- routers/auth.router.js
+++ routers/auth.router.js
@@ -1,12 +1,13 @@
 const { Router } = require("express");
-const { signIn, signUp } = require("../controllers/auth.controller");
-const { checkExistEmail } = require("../middlewares/user.middleware");
+const { signIn, signUp, forgetPassword } = require("../controllers/auth.controller");
+const { checkExistEmail, sendEmailForgetPassword } = require("../middlewares/user.middleware");
 const { User } = require("../models");
 
 const authRouter = Router();
 
 authRouter.post("/sign-in", checkExistEmail(User), signIn);
 authRouter.post("/sign-up", signUp);
+authRouter.post("/forget-password",checkExistEmail(User), sendEmailForgetPassword(), forgetPassword)
 
 module.exports = {
   authRouter,
