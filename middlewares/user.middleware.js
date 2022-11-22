e00b6d83afb0a0ba53025575044877e6d9ae9160
Dương Doãn <tranduongdoan0922@gmail.com>
2022-11-22 17:13:47 +0700

    refactor

--- middlewares/user.middleware.js
+++ middlewares/user.middleware.js
@@ -2,25 +2,25 @@ const createHttpError = require("http-errors");
 const { ACCOUNT_IS_NOT_EXIST } = require("../utils/constants/auth.constant");
 
 const checkExistEmail = (Model) => {
-    return async (req,res,next) => {
-      try {
-        const { email } = req.body;
-        const isExist = await Model.findOne({
-            where: {
-                email
-            }
-        });
-        if( isExist ) {
-             req.recordDB = isExist;
-             next();
-        } else {
-            next(createHttpError(404, ACCOUNT_IS_NOT_EXIST))
+  return async (req,res,next) => {
+    try {
+      const { email } = req.body;
+      const isExist = await Model.findOne({
+        where: {
+          email
         }
-      } catch (error) {
-        next(createHttpError(500, error))
+      });
+      if(isExist){
+        req.recordDB = isExist;
+        next();
+      } else {
+        next(createHttpError(404, ACCOUNT_IS_NOT_EXIST))
       }
+    } catch (error) {
+      next(createHttpError(500, error))
     }
   }
+}
   
   module.exports = {
     checkExistEmail
